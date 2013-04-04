01c81d8aca1cc9ea5672b42de4d9e6ed74af70aa
Mark Richter <mrichter@thoughtworks.com>
2013-04-04 14:15:38 -0700

    nothing but enable-psremoting now

--- script.ps1
+++ script.ps1
@@ -4,21 +4,5 @@ param(
 	)
 
 Write-Host "Beging installing IIS features..."
+
 Enable-PSRemoting –force
-Import-Module .\OSFeatures.psm1
-
-#$result = Test-PSRemoting -ComputerName 08Web
-
-#Write-Host $result
-
-#if ($result -ne $true)
-#{
-#	Write-Host "PowerShell remoting on $ComputerName is not enabled. It needs to be."
-#	return $result
-#}
-
-#Get-OSFeature -ComputerName 08WEB >> .\windows_features.txt
-
-#Write-Host "...finished installing IIS features."
-
-#return $result
