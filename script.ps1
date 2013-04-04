4c1bb43e7383cec66a76b7d53345ed44f1a2448f
Mark Richter <mrichter@thoughtworks.com>
2013-04-04 16:51:53 -0700

    try returning -1

--- script.ps1
+++ script.ps1
@@ -12,12 +12,12 @@
 		$result = Test-PSRemoting -ComputerName $ComputerName
 		if ($result -eq $False) { 
 			Write-Host $result
-			Exit 1 
+			Exit -1 
 		}
 		Add-OSFeature -ComputerName $ComputerName -FeatureName Web-Server
 	}
 	catch {
 		Write-Host $_
-		Exit 1
+		Exit -1
 	}
 	
