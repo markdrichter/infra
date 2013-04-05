346472c75e679222bc517f8a87ef634ab3c2a0b0
Mark Richter <mrichter@thoughtworks.com>
2013-04-05 08:16:44 -0700

    force exit 1

--- script.ps1
+++ script.ps1
@@ -9,16 +9,13 @@
 	try {
 	
 		$ErrorActionPreference = "Stop"
-		$result = Test-PSRemoting -ComputerName $ComputerName
-		if ($result -eq $False) { 
-			Write-Host $result
-			[Environment]::SetEnvironmentVariable("ERRORLEVEL", "1", "User")
-			Exit
-		}
+		Test-PSRemoting -ComputerName $ComputerName
+		Write-Host $?
+		Exit 1
 		Add-OSFeature -ComputerName $ComputerName -FeatureName Web-Server
 	}
 	catch {
 		Write-Host $_
-		Exit -1
+		Exit 1
 	}
 	
